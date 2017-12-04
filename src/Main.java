cd72ab76156d384173140f8401e7db61dd682209
Caio Batista <caioafb@gmail.com>
2017-12-04 10:44:23 -0300

    Inicio da memoria

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		MemoriaPrincipal memoria = new MemoriaPrincipal();
+		
+		memoria.mostrarMemoria();
+		
+		System.out.println(memoria.getValueReg(1));
+
+	}
+
+}
--- src/MemoriaPrincipal.java
+++ src/MemoriaPrincipal.java
@@ -1,4 +1,32 @@
+import java.util.ArrayList;
 
 public class MemoriaPrincipal {
-
+	
+	private static int TAMANHO = 8;
+	private ArrayList<Integer> memoria;
+	
+	public MemoriaPrincipal() {
+		this.memoria = new ArrayList<Integer>(TAMANHO);
+	
+		for (int i = 0; i < TAMANHO; i++) {
+			memoria.add(0);
+		}
+	
+	}
+	
+	public void mostrarMemoria() {
+		System.out.println("- Memória -");
+		for (int i = 0; i < TAMANHO; i++) {
+			System.out.println("R" + i + " - " + "[" + memoria.get(i) + "]");
+		}
+		System.out.println("-----------");
+	}
+	
+	public int getValueReg(int reg) {
+		
+		return memoria.get(reg);
+		
+	}
+	
+	
 }
